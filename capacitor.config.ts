1a55f9389b42dfb64f0a1193b57e0b45d207df32
Richard <richardroj@gmail.com>
2024-10-01 13:56:40 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'a9i-bc',
+  webDir: 'www'
+};
+
+export default config;
